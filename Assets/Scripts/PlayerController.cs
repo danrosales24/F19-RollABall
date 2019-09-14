144d241940049c7b62851a8ef43623345106046b
danrosales24 <52152236+danrosales24@users.noreply.github.com>
2019-09-13 20:59:52 -0400

    Added Script (still not finished)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-        
+        Input.
     }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
+
